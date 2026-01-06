95cc35d3679b865f4c94441e87c561a4ac4d5178
Devvrat Hans <devvrat.coding@gmail.com>
2026-01-06 22:00:57 +0530

    Create 3379-ScoreOfAString

---
+++ 3379-ScoreOfAString/3379-ScoreOfAString.cpp
@@ -0,0 +1,15 @@
+// Last updated: 1/6/2026, 10:00:57 PM
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int score = 0;
+        int n = s.size();
+
+        for (int i = 1; i<n; i++){
+            score += abs(int(s[i]) -int(s[i-1]));
+        }
+
+        return score;
+        
+    }
+};
