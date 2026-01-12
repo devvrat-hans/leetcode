3bc569e5b44856b7f8d056aa5861d08214d924be
Devvrat Hans <devvrat.coding@gmail.com>
2026-01-12 09:23:59 +0530

    Create 85-MaximalRectangle

---
+++ 85-MaximalRectangle/85-MaximalRectangle.cpp
@@ -0,0 +1,51 @@
+// Last updated: 1/12/2026, 9:23:59 AM
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int maxArea = 0;
+        int n = heights.size();
+
+        for (int i = 0; i <= n; i++) {
+            int currHeight = (i == n ? 0 : heights[i]);
+
+            while (!st.empty() && currHeight < heights[st.top()]) {
+                int h = heights[st.top()];
+                st.pop();
+
+                int width;
+                if (st.empty())
+                    width = i;
+                else
+                    width = i - st.top() - 1;
+
+                maxArea = max(maxArea, h * width);
+            }
+            st.push(i);
+        }
+        return maxArea;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if (matrix.empty()) return 0;
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        vector<int> heights(cols, 0);
+        int ans = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1')
+                    heights[j]++;
+                else
+                    heights[j] = 0;
+            }
+
+            ans = max(ans, largestRectangleArea(heights));
+        }
+
+        return ans;
+    }
+};
